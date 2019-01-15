bd40c58c645270e0cd261974591d2d02437f3a39
Dave <you@example.com>
2019-01-15 22:40:41 +0000

    (Slightly wonky) sidebar

---
+++ src/sidebar.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import { colours, shadows } from "./styles";
+
+export interface SideBarProps {
+  toggleGroup: (groupName: string) => void;
+  selectedGroups: string[];
+  groupNames: string[];
+}
+export const SideBar = ({
+  groupNames,
+  toggleGroup,
+  selectedGroups
+}: SideBarProps) => {
+  return (
+    <div>
+      <h1
+        style={{
+          backgroundColor: colours.spellDividerBackground,
+          boxShadow: shadows.standard,
+          margin: "0em 1em 0em 0.2em",
+          padding: "0.2em"
+        }}
+      >
+        Class / Subclass
+      </h1>
+      {groupNames.sort().map((g, i) => {
+        const id = `choose-filter-${g}`;
+
+        return (
+          <div
+            key={g}
+            style={{
+              backgroundColor: colours.cardBackground,
+              fontSize: "1.3em",
+              margin: "0.8em",
+              padding: "0.2em",
+              boxShadow: shadows.standard
+            }}
+          >
+            <input
+              id={id}
+              type="checkbox"
+              onChange={() => toggleGroup(g)}
+              checked={selectedGroups.indexOf(g) > -1}
+              style={{
+                margin: "0.8em",
+                transform: "scale(1.5)"
+              }}
+            />
+            <label htmlFor={id}>{g}</label>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
--- src/spell-list.tsx
+++ src/spell-list.tsx
@@ -1,6 +1,7 @@
 import { produce } from "immer";
 import { groupBy } from "lodash";
 import * as React from "react";
+import { SideBar } from "./sidebar";
 import { SpellCard } from "./spell-card";
 import { groupSpellsKnownBySpell, spellLevelText } from "./spell-utils";
 import { colours, shadows } from "./styles";
@@ -11,8 +12,15 @@ interface SpellListProps {
   spellsKnown: SpellsKnown[];
 }
 interface SpellListState {
+  showSidebar: boolean;
   searchText: string;
+  groupFilter: string[];
 }
+
+/**
+ * Alias for a spell name to class mapping
+ */
+type SpellGrouping = Record<string, string[]>;
 
 /**
  * A (filtered) listing of all spells in the spell book
@@ -24,19 +32,29 @@ export default class SpellList extends React.Component<
   constructor(props: SpellListProps) {
     super(props);
     this.state = {
-      searchText: ""
+      searchText: "",
+      groupFilter: [],
+      showSidebar: false
     };
   }
 
   public render() {
-    const matchingSpells = this.props.spellList.filter(
-      this.spellMatchesText.bind(this, this.state.searchText)
-    );
+    const groupedBySpell = groupSpellsKnownBySpell(this.props.spellsKnown);
+
+    const matchingSpells = this.props.spellList
+      .filter(this.spellMatchesText.bind(this, this.state.searchText))
+      .filter(
+        this.spellMatchesGroupFilter.bind(
+          this,
+          this.state.groupFilter,
+          groupedBySpell
+        )
+      );
 
     const spellGroups = groupBy(matchingSpells, "level");
 
     const groupedCards = Object.keys(spellGroups).map(groupName =>
-      this.renderSpellLevel(groupName, spellGroups[groupName])
+      this.renderSpellLevel(groupName, groupedBySpell, spellGroups[groupName])
     );
 
     return (
@@ -48,28 +66,8 @@ export default class SpellList extends React.Component<
           backgroundColor: colours.pageBackground
         }}
       >
-        <div
-          style={{
-            position: "sticky",
-            top: 0,
-            backgroundColor: colours.topNavBackground,
-            padding: "0.5em 0em",
-            boxShadow: shadows.standard
-          }}
-        >
-          <input
-            style={{
-              height: "2.5em",
-              width: "96%",
-              border: "none",
-              margin: "0% 2%",
-              paddingLeft: "1em"
-            }}
-            placeholder={"Search"}
-            value={this.state.searchText}
-            onChange={e => this.setSearchText(e.target.value)}
-          />
-        </div>
+        {this.renderSidebar()}
+        {this.renderTopBar()}
 
         <div
           style={{
@@ -82,6 +80,25 @@ export default class SpellList extends React.Component<
     );
   }
 
+  private toggleGroupFilter(groupName: string) {
+    this.setState(
+      produce(this.state, draft => {
+        draft.groupFilter =
+          draft.groupFilter.indexOf(groupName) > -1
+            ? draft.groupFilter.filter(group => group !== groupName)
+            : draft.groupFilter.concat(groupName);
+      })
+    );
+  }
+
+  private toggleSidebar() {
+    this.setState(
+      produce(this.state, draft => {
+        draft.showSidebar = !draft.showSidebar;
+      })
+    );
+  }
+
   /**
    * Does a spell match a text value.
    */
@@ -91,6 +108,22 @@ export default class SpellList extends React.Component<
       toMatch.toLowerCase().indexOf(searchTerm) > -1;
 
     return matchesString(spell.name) || spell.description.some(matchesString);
+  }
+
+  /**
+   * Does a spell match the selected groups
+   */
+  private spellMatchesGroupFilter(
+    selectedGroups: string[],
+    groupedBySpell: SpellGrouping,
+    spell: Spell
+  ): boolean {
+    if (selectedGroups.length === 0) {
+      return true;
+    }
+
+    const spellKnownBy = groupedBySpell[spell.name];
+    return spellKnownBy.some(knownBy => selectedGroups.indexOf(knownBy) > -1);
   }
 
   /**
@@ -111,9 +144,11 @@ export default class SpellList extends React.Component<
     return a.level - b.level || a.name.localeCompare(b.name);
   }
 
-  private renderSpellLevel(level: string, spells: Spell[]) {
-    const groupedBySpell = groupSpellsKnownBySpell(this.props.spellsKnown);
-
+  private renderSpellLevel(
+    level: string,
+    groupedBySpell: SpellGrouping,
+    spells: Spell[]
+  ) {
     const spellCards = spells
       .sort(this.compareSpells)
       .map(spell => (
@@ -148,6 +183,75 @@ export default class SpellList extends React.Component<
         >
           {spellCards}
         </div>
+      </div>
+    );
+  }
+
+  private renderSidebar() {
+    // TODO - Animate this
+    if (!this.state.showSidebar) {
+      return null;
+    }
+
+    return (
+      <div
+        style={{
+          position: "absolute",
+          width: "30%",
+          height: "100%",
+          backgroundColor: colours.topNavBackground,
+          boxShadow: shadows.standard
+        }}
+      >
+        <div
+          style={{
+            paddingTop: "6em"
+          }}
+        >
+          <SideBar
+            selectedGroups={this.state.groupFilter}
+            groupNames={this.props.spellsKnown.map(o => o.knownBy)}
+            toggleGroup={this.toggleGroupFilter.bind(this)}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  private renderTopBar() {
+    return (
+      <div
+        style={{
+          position: "sticky",
+          top: 0,
+          backgroundColor: colours.topNavBackground,
+          padding: "0.5em 0em",
+          boxShadow: shadows.standard
+        }}
+      >
+        <button
+          onClick={this.toggleSidebar.bind(this)}
+          style={{
+            width: "5em",
+            margin: "0.5em 1em",
+            padding: "0.2em",
+            border: "1px outset blue"
+          }}
+        >
+          Sidebar
+        </button>
+        <input
+          style={{
+            height: "2.5em",
+            margin: "0.5em 1em",
+            width: "90%",
+            border: "none",
+            paddingLeft: "0.2em"
+          }}
+          placeholder={"Search"}
+          value={this.state.searchText}
+          onChange={e => this.setSearchText(e.target.value)}
+        />
       </div>
     );
   }
